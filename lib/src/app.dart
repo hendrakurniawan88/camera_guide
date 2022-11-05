ec4b26f83d0681447402ce6d522b196d93cfe78c
isaylhendra <isaylhendra@gmail.com>
2022-11-05 21:48:12 +0700

    main

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
 
-import 'router/app_router.dart';
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark, primaryColor: Colors.blueGrey),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('MaterialApp Theme'),
+        ),
       ),
-      routeInformationParser: appRouter.routeInformationParser,
-      routerDelegate: appRouter.routerDelegate,
-      routeInformationProvider: appRouter.routeInformationProvider,
     );
   }
 }
